3589fde4dc7fdd30931574e67af3ef060c5e17cf
YuraY22 <yurayashchuk22@gmail.com>
2023-03-06 11:26:36 -0600

    Created a feature, step definition, and login page for user story 1

---
+++ src/test/java/com/stellan/pages/LoginPage.java
@@ -0,0 +1,25 @@
+package com.stellan.pages;
+
+import com.stellan.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(name="USER_LOGIN")
+    public WebElement username;
+
+    @FindBy(name="USER_PASSWORD")
+    public  WebElement password;
+
+    @FindBy(xpath = "//input[@type='submit']")
+    public WebElement loginButton;
+
+
+
+}
