79841ccda6b4ca94ed2cdebbdead36637d7e5f4d
urko23 <urko23@yahoo.com>
2023-03-07 22:03:55 -0600

    Add US03_14_Yuriy Feature, and StepDef

---
+++ src/test/java/com/stellan/step_definitions/US14_Yuriy_StepDef.java
@@ -0,0 +1,35 @@
+package com.stellan.step_definitions;
+
+import com.stellan.pages.Company_btn_menu;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class US14_Yuriy_StepDef {
+
+    Company_btn_menu companyBtnMenu = new Company_btn_menu();
+
+    @When("users click the Company module")
+    public void usersClickTheCompanyModule() {
+
+        companyBtnMenu.company_btn.click();
+
+    }
+
+    @Then("verify the users see flowing 7 options:")
+    public void verify_the_users_see_flowing_options(List<String> expectedList) {
+
+        List<String> actualList = new ArrayList<>(); // new empty List
+
+        for (WebElement each : companyBtnMenu.CompanyBtnList) {
+            actualList.add(each.getText());
+        }
+        Assert.assertEquals(expectedList, actualList);
+
+
+    }
+}
