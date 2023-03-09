887beb0ae6f89f4443f207a621157dd95661cf42
Asbreez <aiyaiai@hotmail.com>
2023-03-09 12:22:24 -0600

    Made some changes

---
+++ src/test/java/com/stellan/step_definitions/US10_StepDef_Ai.java
@@ -0,0 +1,14 @@
+package com.stellan.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US10_StepDef_Ai {
+    @When("User clicks on the Driver icon on the homepage")
+    public void userClicksOnTheDriverIconOnTheHomepage() {
+    }
+
+    @Then("Verify if user see all following {int} <modules>:")
+    public void verifyIfUserSeeAllFollowingModules(int arg0) {
+    }
+}
