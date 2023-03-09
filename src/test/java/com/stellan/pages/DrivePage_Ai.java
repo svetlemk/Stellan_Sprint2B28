fc1d83b343e9bdca1fd1cd3d4486449e7a47f144
Asbreez <aiyaiai@hotmail.com>
2023-03-09 14:57:27 -0600

    Made some changes

--- src/test/java/com/stellan/pages/DrivePage_Ai.java
+++ src/test/java/com/stellan/pages/DrivePage_Ai.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DrivePage_Ai {
+
+
     public void DrivePage_Ai(){
         PageFactory.initElements(Driver.getDriver(),this);
 
