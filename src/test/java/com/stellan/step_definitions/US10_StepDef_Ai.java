6757db42fdb116e4e911c5e85bb5aeadc2484dd6
Asbreez <aiyaiai@hotmail.com>
2023-03-09 14:44:34 -0600

    Made a name change to the file

--- src/test/java/com/stellan/step_definitions/US10_StepDef_Ai.java
+++ src/test/java/com/stellan/step_definitions/US10_StepDef_Ai.java
@@ -1,15 +1,27 @@
 package com.stellan.step_definitions;
 
+import com.stellan.pages.DrivePage_Ai;
+import com.stellan.utilities.BrowserUtils;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.List;
 
 public class US10_StepDef_Ai {
+
+    DrivePage_Ai drivePage_ai = new DrivePage_Ai();
     @When("User clicks on the Driver icon on the homepage")
     public void userClicksOnTheDriverIconOnTheHomepage() {
+        drivePage_ai.driverPageIcon.click();
     }
 
     @Then("Verify if user sees all following {int} <modules>:")
-    public void verifyIfUserSeeAllFollowingModules(int arg0) {
+    public void verifyIfUserSeeAllFollowingModules(List<String> expectedList) {
+        List<String> actualList = BrowserUtils.getElementsText(drivePage_ai.actualListWebElements);
+        Assert.assertEquals(expectedList,actualList);
+
+
     }
 
 }
