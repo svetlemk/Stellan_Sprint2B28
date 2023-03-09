2c96b5f2593c7de38c95f1078f5835b8a35125a6
Asbreez <aiyaiai@hotmail.com>
2023-03-09 14:43:33 -0600

    Made a name change to the file

--- src/test/java/com/stellan/pages/DrivePage_Ai.java
+++ src/test/java/com/stellan/pages/DrivePage_Ai.java
@@ -5,9 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.List;
-
-public class DriverPage_Ai {
+public class DrivePage_Ai {
     public void DriverPage_Ai(){
         PageFactory.initElements(Driver.getDriver(),this);
 
