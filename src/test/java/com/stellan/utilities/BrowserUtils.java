0893bf63f2bcb34afe67fd1be73ee5b1fc5e2079
skocherry <svetlemk@gmail.com>
2023-03-09 10:43:36 -0600

    added comment to utility method

--- src/test/java/com/stellan/utilities/BrowserUtils.java
+++ src/test/java/com/stellan/utilities/BrowserUtils.java
@@ -76,7 +76,7 @@ public class BrowserUtils {
 
     }
     public static List<String> getElementsText(List<WebElement> list) {
-        List<String> elemTexts = new ArrayList<>();
+        List<String> elemTexts = new ArrayList<>(); // empty list of Text
         for (WebElement el : list) {
             elemTexts.add(el.getText());
         }
