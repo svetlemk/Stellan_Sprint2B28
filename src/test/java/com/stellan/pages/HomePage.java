c5486810e9fa9de03fb3b464bcf753bb835ade87
skocherry <svetlemk@gmail.com>
2023-03-10 09:55:35 -0600

    adjusted homepage

--- src/test/java/com/stellan/pages/HomePage.java
+++ src/test/java/com/stellan/pages/HomePage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
-public class HomePage extends BasePage {
+public class HomePage extends LoginPage {
 
     public HomePage() {
         PageFactory.initElements(Driver.getDriver(), this);
@@ -26,15 +26,15 @@ public class HomePage extends BasePage {
     @FindBy(xpath = "//div[@class='bx-desktop-appearance-tab']//span/..")  //US08
     public List<WebElement> chatAndCallsOptions;
 
-    @FindBy(xpath ="//a[@title='Time and Reports']")
-    public WebElement TimeAndReports  ;
+    @FindBy(xpath = "//a[@title='Time and Reports']")
+    public WebElement TimeAndReports;
 
-    @FindBy(xpath="//span[@class='main-buttons-item-text-title']")
-    public List<WebElement> TimeAndReportsModules ;
+    @FindBy(xpath = "//span[@class='main-buttons-item-text-title']")
+    public List<WebElement> TimeAndReportsModules;
 
 
-    @FindBy(xpath="(//div[@class='b24-app-block-content'])[1]")   //US04
+    @FindBy(xpath = "(//div[@class='b24-app-block-content'])[1]")   //US04
     public WebElement operationSystems;
 
 
-
+}
